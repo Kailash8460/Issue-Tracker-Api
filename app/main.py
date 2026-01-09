3e452880a7078f67c96c8d70486f2daff199a83a
Kailash8460 <kailashbhagchandani60@gmail.com>
2026-01-09 16:18:13 +0530

    Database connection successfully done.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.database import test_connection
 
 app = FastAPI()
 
@@ -6,3 +7,12 @@ app = FastAPI()
 @app.get("/")
 def health_check():
     return {"status": "Issue Tracker API is running"}
+
+
+@app.get("/test-db-connection")
+def test_db_connection():
+    try:
+        result = test_connection()
+        return {"database_connection": "successful", "result": result}
+    except Exception as e:
+        return {"database_connection": "failed", "error": str(e)}
